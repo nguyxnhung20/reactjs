a556f522db415d87a5e02f8d17b7f7cd41a205bd
nguyxnhung20 <nguyenduyhung.20052002@gmail.com>
2024-03-08 16:48:29 +0700

    [Add] Day3_useEffect

---
+++ src/components/search/childComponent/SearchBox.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './styleSearch.scss';
+
+function SearchBox({ onSearch }) {
+  const [user, setUser] = useState('');
+
+  const findUser = (event) => {
+    setUser(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  return (
+    <div className="searchBox">
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">
+          @
+        </span>
+        <input
+          type="text"
+          value={user}
+          className="form-control"
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          onChange={(event) => findUser(event)}
+        />
+      </div>
+      <div className="checkbox">
+        <input type="checkbox" />
+        Only show products in stock
+      </div>
+    </div>
+  );
+}
+
+export default SearchBox;
